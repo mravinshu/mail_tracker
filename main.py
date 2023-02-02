be1bda73e4c3d6ca4d7151958be7ed934469177a
Ravinshu Makkar <55399637+mravinshu@users.noreply.github.com>
2023-02-02 23:17:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import datetime
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class tracker(Resource):
+
+    def get(self):
+        return {'msg': 'Hello World'}
+
+    def post(self):
+        payload = request.get_json()
+        mail_id = payload['mail_id']
+        TrackerTime().add_time_to_file(mail_id)
+        return {'msg': 'recived mail_id: {}'.format(mail_id)}
+
+
+class TrackerTime:
+    def add_time_to_file(self, mail_id):
+        with open('time.txt', 'a') as f:
+            f.write(str(mail_id) + '\n')
+            f.write(str(datetime.datetime.now()) + '\n')
+            f.write('------------------------' + '\n')
+
+
+api.add_resource(tracker, '/')
+
+
+if __name__ == '__main__':
+    app.run()
